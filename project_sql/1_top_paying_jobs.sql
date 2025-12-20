3cdf3d6f517f4bbc2897e2674fa53321f95cd685
Zach <youremail@example.com>
2025-12-20 01:17:37 -0500

    upload sql files

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,25 @@
+SELECT 
+    name AS company_name,
+    skill_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+LEFT JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+WHERE 
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10
+
+
+    
+
+
+
